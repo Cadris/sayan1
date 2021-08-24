9a818e9fc03aff6f03c4605b2afc81c6708557cf
Cadris <cadrissilver@gmail.com>
2021-08-25 00:03:50 +0530

    passing and handling data as a obserable

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Task } from 'src/data/Task';
 import { TASKS } from 'src/data/mock-tasks';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,8 @@ export class TaskService {
 
   constructor() { }
 
-  getTask(): Task[]{
-    return TASKS;
+  getTask(): Observable<Task[]>{
+    const tasks = of(TASKS)
+    return tasks;
   }
 }
