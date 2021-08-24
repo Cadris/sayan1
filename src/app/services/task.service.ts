eed46d1e94f662edc94f0461d520001e11595afa
Cadris <cadrissilver@gmail.com>
2021-08-25 00:44:54 +0530

    Getting data from the server

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Task } from 'src/data/Task';
-import { TASKS } from 'src/data/mock-tasks';
+//import { TASKS } from 'src/data/mock-tasks';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/Tasks';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getTask(): Observable<Task[]>{
-    const tasks = of(TASKS)
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
