efa4e578477efd100bf59c7c0733b5f8f858a7dd
Cadris <cadrissilver@gmail.com>
2021-08-27 01:44:24 +0530

    Task add to server and display

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -31,7 +31,8 @@ export class TasksComponent implements OnInit {
   }
 
   addTask(task: Task){
-    console.log(task);
-    
+    this.taskService.addTask(task).subscribe((task) =>{
+      this.tasks.push(task);
+    });
   }
 }
